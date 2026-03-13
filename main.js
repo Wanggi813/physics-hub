7618fef9adb4c843bace7ce92717a2eda6061c64
Wanggi813 <sufaman0@gmail.com>
2026-03-13 15:44:17 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ const projects = [
   { title: "나만의 별 키우기", category: "현대물리", desc: "가스를 모아 별을 점화하라! 중력과 압력의 균형(정역학 평형) 게임", tags: ["핵융합", "별의진화", "흑체복사"], emoji: "🌟", demo: "./simul/별의 탄생.html", curriculumId: "[12전자03-05]", thumb: "./thumb_nail/별의 탄생.png" },
 
  // ===== 현대물리 =====
-  { title: "RLC 공명", category: "현대물리", desc: "베타붕괴 현상 알아보기", tags: ["베타붕괴", "입자물리"], emoji: "☢️", demo: "./simul/베타붕괴.html", curriculumId: " ", thumb: "./thumb_nail/베타붕괴.png" },
+  { title: "베타붕괴", category: "현대물리", desc: "베타붕괴 현상 알아보기", tags: ["베타붕괴", "입자물리"], emoji: "☢️", demo: "./simul/베타붕괴.html", curriculumId: " ", thumb: "./thumb_nail/베타붕괴.png" },
   { title: "입자가속기", category: "현대물리", desc: "입자가속기 속에서 생기는 표준모형 알아보기", tags: ["입자가속기", "입자물리"], emoji: "🌀", demo: "./simul/입자가속기.html", curriculumId: " ", thumb: "./thumb_nail/입자가속기.png" },
 
 
@@ -798,4 +798,5 @@ render();
       closeModal();
     }
   });
+
 })();
